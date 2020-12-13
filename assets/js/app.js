9814023a83ff2f2caeb71e462987d38dd433094d
Leo Terry <lucasenriquez1997@gmail.com>
2020-12-13 19:43:25 -0300

    error funcion repetida

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,43 +2,35 @@
 let boton = document.querySelector(".fa-arrow-circle-down")
 
 
-boton.addEventListener("click",()=>{
-    window.scrollTo({left:0,top:600,behavior:"smooth"})
+boton.addEventListener("click", () => {
+    window.scrollTo({ left: 0, top: 600, behavior: "smooth" })
 })
 
 
 //BARRA
-
+let btnUp = document.querySelector(".fa-arrow-circle-up")
 let prevScroll = window.pageYOffset;
 
-window.onscroll = ()=>{
+window.onscroll = () => {
     let actualScroll = window.pageYOffset;
-    if (prevScroll >= actualScroll){
+    if (prevScroll >= actualScroll) {
         document.querySelector(".navbar").style.top = "0px";
         document.querySelector(".navbar").style.position = "sticky";
-        
+        btnUp.style.display = "inline-block"
+        btnUp.style.right = "30px";
 
-    }else{
+
+    } else {
         document.querySelector(".navbar").style.top = "-200px";
+        btnUp.style.right = "-60px"
 
     }
     prevScroll = actualScroll
 }
 
 //SUBIR
-let btnUp = document.querySelector(".fa-arrow-circle-up")
 
-window.onscroll = function() {
-    let scroll = document.documentElement.scrollTop;
 
-    if (scroll > 500){
-        btnUp.style.display= "inline-block"
-        btnUp.style.right = "30px";
-    }else{
-        btnUp.style.right = "-60px"
-    }
-}
-btnUp.addEventListener("click",()=>{
-    window.scrollTo({left:0,top:0,behavior:"smooth"})
+btnUp.addEventListener("click", () => {
+    window.scrollTo({ left: 0, top: 0, behavior: "smooth" })
 })
-
